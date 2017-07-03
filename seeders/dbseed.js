fa0ed65380e57d92157c8ff85e497e87c1e609d2
jdisser <jimdisser@altocirrusapps.com>
2017-07-03 00:52:31 +0000

    added state, energy, and voltage logic to battery model

--- seeders/dbseed.js
+++ seeders/dbseed.js
@@ -48,7 +48,8 @@ function Panel() {
 *  and adjust the battery voltage based on the stored charge to simulate
 *  charge cycles. The model is a simple approximation for generating some
 *  interesting seed data.
-*  The model assumes the time period of the charging is 1 hour.
+*  The battery maintains it's state and statistics
+*  for discharge cycles, charge, voltage, and energy.
 ***/
 function Battery(){ 
     
@@ -58,24 +59,31 @@ function Battery(){
         DISCHARGE:  3
     };
     
+    var lastStates = [states.INIT, states.INIT, states.INIT];   //last 3 states to debounce charge cycles
     
     this.batV = 12700;              //battery voltage in mV
     this.batCapacity = 220000;      //capacity in mAH
     this.batCharge = 176000;        //battery charge initially at 80% for simulation
-    this.chargeState = states.INIT;
+    this.chargeState = states.INIT; //battery is initially not charging or discharging
+    this.ahCumulative = 0;          //total charge
     
+    this.energy = {
+        charge: 0,
+        discharge: 0
+    };
+    
+    this.voltage = {
+        max: 0,
+        min: 0
+    };
 
     
-    var lastStates = [states.INIT, states.INIT, states.INIT];   //last n states to debounce charge cycles
-    
-//# of battery charging cycles could be > solar cycles
-    
-    var discharges = {              //object to track discharge statistics
-      min = 0,
-      last = 0,
-      sum = 0,
-      n = 0,                //# of battery charging cycles could be > solar cycles
-      avg = 0
+    this.dischargeStat = {              //object to track discharge statistics
+      min: 0,
+      last: 0,
+      sum: 0,
+      n: 0,                //# of battery charging cycles could be > solar cycles
+      avg: 0
     };
     
     var setCycles = function(currentState) {
@@ -100,40 +108,76 @@ function Battery(){
             }
         });
         
-        // if vote != to chargeState then= vote
+        
+        if ((debState !== this.chargeState)&&(debState !== -1)){    // if debState != to chargeState then a new state is starting
+            if (debState === states.CHARGE) {                       //the discharge cycle is ending here
+                this.chargeState = states.CHARGE;
+                                                            //service the discharge object
+                this.dischargeStat.n++;                     //increment the number of discharge cycles
+                this.dischargeStat.sum += this.batCharge;   //add to the sum (BIG NUMBER!!)
+                this.dischargeStat.last = this.batCharge;
+                this.dischargeStat.avg = Math.floor(this.dischargeStat.sum / this.dischargeStat.n);
+                if (this.batCharge < this.dischargeStat.min){
+                    this.dischargeStat.min = this.batCharge;
+                }
+                
+            } else {                                        //the charge cycle is ending here
+                this.chargeState = states.DISCHARGE;        //only 2 reachable states after INIT
+                
+            }
+        }
+        
     };
 
-    this.chargeBatV = function(batI){
+    this.chargeBatV = function(ah){
         
         var currentState;
+        var deltaV = Math.floor(ah/this.batCapacity * 2.5); //crude approximation!
+        var deltaE = Math.floor(this.batV * ah / 10000000); //mv ->V /1000 * maH ->A /1000 -> W -> (kW) /1000 -> (.01kW) * 100
         
-        if (batI > 0){
+        if (ah > 0){
             if (this.batV < 13.2){
-                this.batV += batI / 1000;
+                this.batV += deltaV;
                 currentState = states.CHARGING;
+                this.energy.charge += deltaE;
             }
         } else {
             if (this.batV > 10.7){
-                this.batV -= batI / 1000;
+                this.batV -= deltaV;
                 currentState = states.DISCHARGING;
+                this.energy.discharge -= deltaE;
             }
+        }
+        if (this.batV < this.voltage.min){
+            this.voltage.min = this.batV;
+        }
+        if (this.batV > this.voltage.max){
+            this.voltage.max = this.batV;
         }
         this.setCycles(currentState);
     }
     
-    this.charge = function(iIn){
-        if (this.batCharge <= (this.batCapacity - iIn)) {
-            this.batCharge += iIn;
-            chargeBatV(iIn);
-            return this.batCharge;
+    var ampHours = function(current, interval){
+        var hours = interval.asHours();
+        return Math.floor(current * hours); //returns mAH
+    }
+    
+    
+    this.charge = function(iIn, duration){
+        var ah = ampHours(iIn, duration);
+        if (this.batCharge <= (this.batCapacity - ah)) {
+            this.batCharge += ah;
+            chargeBatV(ah);
+            //return this.batCharge;
         }
     }
     
-    this.discharge = function(iOut){
-        if (this.batCharge > iOut) {
-            this.batCharge -= iOut;
-            chargeBatV(-iOut);
-            return this.batCharge;
+    this.discharge = function(iOut, duration){
+        var ah = ampHours(iIn, duration);
+        if (this.batCharge > ah) {
+            this.batCharge -= ah;
+            chargeBatV(-ah);
+            //return this.batCharge;
         }
     }
     
@@ -141,10 +185,7 @@ function Battery(){
         return this.batCharge/this.batCapacity * 100;
     }
     
-    this.batPower = function(batI){
-        return (batI * this.batV) / 10000000;  
-        // mA / 1000 x mV / 1000 /1000 x 100 = .01kWH 
-    }
+
 };
 
 /*
@@ -222,7 +263,7 @@ function Monitor(){
     this.genData = function(){
         //This is the time loop...
         var loops = 0;
-        while (this.now.isBefore(stopTime)){
+        while (this.now.isBefore(this.stopTime)){
             ++loops;
             if (loops > this.count) {
             break;
@@ -230,7 +271,7 @@ function Monitor(){
 
             //code for generating data records within the Monitor goes here    
 
-            this.now.add(interval); //iterate the time loop here
+            this.now.add(this.interval); //iterate the time loop here
         }
         
     };
