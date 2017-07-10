3ebfa91773426c0770c5681f37aa53225398c91d
jdisser <jimdisser@altocirrusapps.com>
2017-07-10 00:25:05 +0000

    Revised Monitor interface

--- models/monitor.js
+++ models/monitor.js
@@ -18,38 +18,39 @@ function Monitor(){
     this.battery = new Battery();
     this.panel = new Panel();
     this.load = new Load();
-    this.now = moment();
-    this.count = 130;               //max number of records
-    this.days = 5;                  //days in simulation
+    this.now = moment();            //the time the monitor was created also the simulation start
+    this.count = 130;               //max number of records, this is set according to n
+//    this.days = 5;                  //days in simulation
     this.minutes = 60;              //min between readings
-    this.interval = moment.duration(this.minutes,'minutes');
-    this.stopTime = moment(now).add(this.days,'days');
+    this.interval = moment.duration(this.minutes,'minutes'); //a duration equal to minutes
+    this.simDuration;               //a duration equal to the length of the simulation
+    this.stopTime;                  //a moment equal to the time the simulation ends
     
-    var batteryData = {
-      timeIndex: 0,
-      batV: 0,
-      batI: 0,
-      batP: 0,
-      batE: 0,
-      bQtotal: 0,
-      bC: 0
+    function BatteryData() {
+      this.timeIndex = 0;
+      this.batV = 0;
+      this.batI = 0;
+      this.batP = 0;
+      this.batE = 0;
+      this.bQtotal = 0;
+      this.bC = 0;
     };
     
     var batteryRecords = [];
     
-    var systemData = {
-      timeIndex: 0,
-      eCharged: 0,
-      eDischarged: 0,
-      ahCumulative: 0,
-      vBatMin: 0,
-      vBatMax: 0,
-      minDischarge: 0,
-      lastDischarge: 0,
-      avgDischarge: 0,
-      discharges: 0,
-      cycles: 0
-    };
+    function SystemData() {
+      this.timeIndex = 0;
+      this.eCharged = 0;
+      this.eDischarged = 0;
+      this.ahCumulative = 0;
+      this.vBatMin = 0;
+      this.vBatMax = 0;
+      this.minDischarge = 0;
+      this.lastDischarge = 0;
+      this.avgDischarge = 0;
+      this.discharges = 0;
+      this.cycles = 0;
+    }
     
     var sysRecords = [];
 
@@ -72,28 +73,30 @@ function Monitor(){
       console.log('Initializing monitor code is running');  
     };
     
-    var genBatData = function(){
-        
-    };
     
-    var genSysData = function(){
-        
-    };
+/*    
+* genData creates n battery data records and a system record
+* for each time the simulation passes midnight plus an initial
+* record with the status of the system data after the first
+* simulation call to the battery object.
+* The records are stored in two arrays as objects and each array
+* is returned. (is this the best way to return multiple objects?)
+*
+***/
+      this.genData = function(n){
+      //This is the time loop...
+      var loops = 0;
+      while (this.now.isBefore(this.stopTime)){
+          ++loops;
+          if (loops > this.count) {
+          break;
+          }
     
-    this.genData = function(){
-        //This is the time loop...
-        var loops = 0;
-        while (this.now.isBefore(this.stopTime)){
-            ++loops;
-            if (loops > this.count) {
-            break;
-            }
-
-            //code for generating data records within the Monitor goes here    
-
-            this.now.add(this.interval); //iterate the time loop here
-        }
-        
+          //code for generating data records within the Monitor goes here    
+    
+          this.now.add(this.interval); //iterate the time loop here
+      }
+      
     };
 }
 
